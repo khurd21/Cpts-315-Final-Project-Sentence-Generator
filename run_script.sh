8aa49afd64318bc3eb19eaa41ee207a9e0c23a59
Kyle Hurd <kyle.hurd@wsu.edu>
2021-11-25 14:11:19 -0800

    Adjusted the source text files and .sh scripts
    - Added more information to the source text files. Currently, I have text for Twilight, Kant, and Hunger Games.
    - Changed the .sh scripts so setup_venv.sh installs the virtual environment and run_script.sh executes it.
    - Adjusted gitignore to ignore the output.s file.

--- run_script.sh
+++ run_script.sh
@@ -1,11 +1,5 @@
-echo "Attempting to install virtualenv...."
-pip3 install virtualenv
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-
-
 echo "Running program...."
-python3 main.py --verbosity > output.s
+source venv/bin/activate
+python3 main.py --verbosity N3 > output.s
 echo "Output delivered to output.s"
 deactivate
---
+++ setup_venv.sh
@@ -0,0 +1,6 @@
+echo "Attempting to install virtualenv...."
+pip3 install virtualenv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+deactivate
